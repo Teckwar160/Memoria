cfbfaaf6854ead3e26313f9e85a883bead2cd36e
Ricardo Peña <teckwar160@gmail.com>
2018-12-11 01:03:01 -0600

    Probando funcion de quitar espacios

--- Memoria/Librerias/archivos.h
+++ Memoria/Librerias/archivos.h
@@ -11,6 +11,8 @@ using namespace std;
 string notifi;
 //Prototipos
 void crear();
+void abrir();
+string quitadorDeEspacios(string palabra);
 string mensaje();
 
 //Funciones
@@ -21,23 +23,80 @@ void crear(){
 	string  nombre;
 	string ruta = "Archivos/";
 	char nombreArchivo[100];
+	char nombreArchivoSE[100];
+	char salida;
 
 	//Programa
 	setlocale(LC_CTYPE,"Spanish");
 
-	system("cls");
-	cout << "¿Com se llamara el archivo?" << endl;
-	cout << "=> ";
-	cin >> nombre;
+	//crea el archivo
+
+	do{
+        system("cls");
+        cout << "Notificacion: " << "No usar espacios estos no se tomaran en cuenta" << endl << endl;
+        cout << "¿Como se llamara el examen?" << endl;
+        cout << "=> ";
+        cin >> nombre;
+
+        do{
+            cout << "\n¿Es correcto? s/n: ";
+            cin  >> salida;
+
+            salida = toupper(salida);
+            fflush(stdin);
+
+
+        }while(salida != 'S' && salida != 'N');
+
+        system("cls");
+	}while(salida != 'S');
+
+	nombre = quitadorDeEspacios(nombre);
 
 	ruta = ruta + nombre + ".txt";
 
 	strcpy(nombreArchivo,ruta.c_str());
 
-	archivo.open(nombreArchivo,ios::app);
+	archivo.open(nombreArchivoSE,ios::app);
 	archivo.close();
 
 	notifi = "El archivo se creo con exito";
+
+
+}
+
+void abrir(){
+
+
+
+
+
+
+}
+
+string quitadorDeEspacios(string palabra){
+
+    int largo;
+    int i,a=0;
+    char palabraChar[100];
+    string palabraSalida;
+
+    strcpy(palabraChar,palabra.c_str());
+
+    largo = strlen(palabraChar);
+    while(palabraChar[a]!='\0'){
+        if (palabraChar[a]==' '){
+            for(i=a;i<largo;i++){
+                palabraChar[i]=palabraChar[i+1];
+            }
+            largo--;
+        }
+        a++;
+    }
+
+    string palabraSalida(palabraChar);
+
+    return palabraSalida;
 }
 
 string mensaje(){
